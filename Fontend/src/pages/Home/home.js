7e25d191909d812cd8118c203f12b959c3e355b4
_nhd.1311 <22520270@gm.uit.edu.vn>
2024-05-22 22:10:13 +0700

    update

--- Fontend/src/pages/Home/home.js
+++ Fontend/src/pages/Home/home.js
@@ -4,17 +4,15 @@ import { Link } from "react-router-dom"
 
 export const Home = () => {
   return (
-    <center className="body">
-      <h1>
-        This is Home Component
-      </h1>
-      <Link to="/login">
-        <button>Login</button>
-      </Link >
-      <Link to="/register">
-        <button>Signup</button>
-      </Link >
-    </center >
+    <div className="home_body">
+      <div className="home_body-nd">
+        <p className="home_body-nd-text">Lưu trữ và bảo mật thông tin cho thành viên gia đình bạn. Đừng để người thân của bạn dần bị lãng quên.</p>
+        <div className="home_body-nd-box">
+          <input type="text" className="home_body-nd-box-search" placeholder="Email của bạn" />
+          <button className="home_body-nd-box-btn">Enter</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
